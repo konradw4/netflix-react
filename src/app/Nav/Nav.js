19135b0acf6e3e8aa7d92da5063fe11dd7e5a1e1
Konrad Wojda <kwojda@sointeractive.pl>
2022-10-08 18:24:24 +0200

    adds nav component

---
+++ src/app/Nav/Nav.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+
+import Logo from "../../assets/netflix-logo.png";
+import Avatar from "../../assets/avatar.jpg";
+import "./Nav.scss";
+
+function Nav() {
+  const [show, setShow] = useState(false);
+
+  const transitionNavBar = () => {
+    console.log('transsss', window.screenY);
+    if (window.scrollY > 100) {
+      setShow(true);
+    } else {
+      setShow(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavBar);
+    return () => window.removeEventListener("scroll", transitionNavBar);
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <div className="nav__contents">
+        <img className="nav__logo" src={Logo} alt="Logo" />
+        <img className="nav__avatar" src={Avatar} alt=""></img>
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
