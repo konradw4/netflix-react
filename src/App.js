bb07723edd92448d900828b6ef6cdb30c327eb64
Konrad Wojda <kwojda@sointeractive.pl>
2022-10-11 18:04:34 +0200

    adds signup + firebase auth

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useEffect } from "react";
 
-import './App.scss';
-import HomeScreen from './app/HomeScreen/HomeScreen';
+import "./App.scss";
+import HomeScreen from "./app/HomeScreen/HomeScreen";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginScreen from "./app/LoginScreen/LoginScreen";
+import { auth } from "./firebase.config";
 
 function App() {
+  const user = null;
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+        if (userAuth) {
+            console.log(userAuth);
+        } else {
+            // logged out
+        }
+    })
+
+    return unsubscribe;
+  }, [])
+
   return (
     <div className="app">
-        <HomeScreen />
+      {!user ? (
+        <LoginScreen />
+      ) : (
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </Router>
+      )}
     </div>
   );
 }
