9361898194c4dd81097c6ac854962b4c4d9330c1
Konrad Wojda <kwojda@sointeractive.pl>
2022-10-17 23:33:18 +0200

    implemented redux - added userSlice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./App.scss";
 import HomeScreen from "./app/components/HomeScreen/HomeScreen";
 import LoginScreen from "./app/components/LoginScreen/LoginScreen";
+import { login, logout, selectUser } from "./app/redux/userSlice";
 import { auth } from "./firebase.config";
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(userAuth => {
-        if (userAuth) {
-            console.log(userAuth);
-        } else {
-            // logged out
-        }
-    })
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
+      } else {
+        // logged out
+        dispatch(logout);
+      }
+    });
 
     return unsubscribe;
-  }, [])
+  }, []);
 
   return (
     <div className="app">
